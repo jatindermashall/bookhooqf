7bbaf7d57ed07fa66021a979651825b755951aa2
jatindermashall <jatindermashall@gmail.com>
2020-06-11 17:29:01 +0530

    mongodb

--- config/database.js
+++ config/database.js
@@ -1,18 +1,18 @@
 module.exports = ({ env }) => ({
-  defaultConnection: 'default',
+  defaultConnection: "default",
   connections: {
     default: {
-      connector: 'bookshelf',
+      connector: "strapi-mongoose",
       settings: {
-        client: 'mysql',
-        host: env('DATABASE_HOST', 'localhost'),
-        port: env.int('DATABASE_PORT', 3306),
-        database: env('DATABASE_NAME', 'bookhooq'),
-        username: env('DATABASE_USERNAME', 'root'),
-        password: env('DATABASE_PASSWORD', ''),
-        ssl: env.bool('DATABASE_SSL', false),
+        client: "mongo",
+        host: env("DATABASE_HOST", "ds017886.mlab.com:17886"),
+        port: env.int("DATABASE_PORT", "17886"),
+        database: env("DATABASE_NAME", "bookhooq"),
+        username: env("DATABASE_USERNAME", "bookhooq"),
+        password: env("DATABASE_PASSWORD", "test123"),
+        ssl: env.bool("DATABASE_SSL", false),
       },
-      options: {}
+      options: {},
     },
   },
 });
